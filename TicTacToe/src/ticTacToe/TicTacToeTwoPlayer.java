c6de4e7eb506ec062f628fbb3473da94994f0e0a
Quang Pham <43323609+quanghuy25899@users.noreply.github.com>
2019-01-24 10:29:46 -0800

    Update TicTacToeTwoPlayer.java

--- TicTacToe/src/ticTacToe/TicTacToeTwoPlayer.java
+++ TicTacToe/src/ticTacToe/TicTacToeTwoPlayer.java
@@ -3,48 +3,34 @@ package ticTacToe;
 import java.util.Scanner;
 import java.util.Random;
 
-/* Complete the following as indicated by the comments.
-Do not delete the comments.
-You will be required to submit a hard copy as well as the zyBook submission.
-*/
-
-/*
-Your name: Quang Pham
-
-Your cNumber: C0472147
-
-*/
-
 public class TicTacToeTwoPlayer {
 	static final int SIZE = 3;
     /*
        Declare a 2-d character array called board with SIZE rows and SIZE columns.
-       Be sure to use the static modifier in the declaration.
     */
 	
-	static final char[][] board = new char[SIZE][SIZE];
+    static final char[][] board = new char[SIZE][SIZE];
 	
     static final char PLAYER1 = 'X';
     static final char PLAYER2 = 'O';
     static Scanner userInput = new Scanner(System.in);
     
     public static void initializeBoard() {
-        /*
-           Initialize each position of the board array to a space character.
-        */
+    /*
+       Initialize each position of the board array to a space character.
+    */
     	
     	for (int rows = 0; rows < SIZE; rows++) {
     		for (int cols = 0; cols < SIZE; cols++) {
     			board[rows][cols] = ' ';
-    			}
+    		}
     	}
         
     }
     
     public static void displayBoard() {
         /*
-            Complete this method so that it displays the tic-tac-toe board
-            to the screen. If a board position is available, that is, if it is
+            If a board position is available, that is, if it is
             a space, output an asterisk followed by a space ("* ".)
         */
     	System.out.println("Board is: ");
@@ -64,11 +50,7 @@ public class TicTacToeTwoPlayer {
     }
     
     /*
-        Write a method called isTied.
-        The method will return true if the game is tied and
-        false otherwise.
-        The method has no formal parameters.
-        The method assumes that there is no winning pattern on the board.
+       The method will return true if the game is tied and false otherwise.
     */
     public static boolean isTied() {
         // check for a row win
@@ -92,26 +74,6 @@ public class TicTacToeTwoPlayer {
  
     
     public static void playerMove(char player) {
-        /*
-            Complete this method so that it makes a move for the
-            player indicated by the formal parameter.
-            The prompts have been supplied for you.
-            Prompt the user to enter a row and column until they
-            enter a legal row and column (The row and column must
-            be between 1 and 3, includsive.)
-            
-            If, once a legal row and column has been entered,
-            the specified position is already occupied (ie not a space)
-            then inform the user and prompt them to enter another
-            row and column.
-    
-            Here are the prompts for you to use:
-       
-            System.out.println("Enter the move for player: " + player);
-            System.out.print("\nRow number: ");
-            System.out.print("\nColumn number: ");
-            System.out.println("That position is taken. Try again.");
-        */
     	boolean done = false;
     	while(done == false) {
 	        System.out.println("Enter the move for player: " + player);
@@ -134,17 +96,10 @@ public class TicTacToeTwoPlayer {
     }
     
     public static char getWinner() {
-        /*
-            Complete this method, getWinner(), so that it returns a character
-            indicating which player has won. If no player has won, the method
-            will return a space.
-            The check for a row win has been done for you.
-            You must write the checks for a column win and a diagnoal win.
-        */
         char whoWon=' ';
         boolean won=false;
         
-        // check for a row win
+        	// check for a row win
 		  for (int i=0; i < SIZE && !won; i++) {
 			if ( (board[i][0] == board[i][1]) && (board[i][1] == board[i][2]) && board[i][0] != ' ') {
 				whoWon = board[i][0];
@@ -160,7 +115,7 @@ public class TicTacToeTwoPlayer {
 	      	   }
 	         }
         
-        // check for a diagonal win
+       		 // check for a diagonal win
 		  if( (board[0][0] == board[1][1]) && (board[1][1] == board[2][2]) && board[0][0] != ' ' ) {
 	        	whoWon = board[0][0];
 				won = true;
@@ -172,11 +127,7 @@ public class TicTacToeTwoPlayer {
 	               
         return(whoWon);
     }
-    
-    /*
-        The main method is complete. Do not change the main method.
-    */
-    
+	
     public static void main(String args[]) {
       
         char whoWon;
